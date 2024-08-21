608c74333d73777616f9eb6388f49a028b74b300
Matt Otto <matt@mattotto.com>
2024-08-20 20:43:16 -0400

    Install xcode theme

--- install.sh
+++ install.sh
@@ -31,6 +31,12 @@ ln -s "$PWD"/zshrc ~/.zshrc
 ln -s "$PWD"/bin ~/bin
 ln -s "$PWD"/dotfiles-local ~/dotfiles-local
 
+XCODE_THEME_INSTALL_PATH=~/Library/Developer/Xcode/UserData/FontAndColorThemes
+XCODE_THEME_NAME=base16-twilight.dark.xccolortheme
+
+[ -e $XCODE_THEME_INSTALL_PATH ] || mkdir -p $XCODE_THEME_INSTALL_PATH
+ln -s "$PWD"/macOS/"$XCODE_THEME_NAME" "$XCODE_THEME_INSTALL_PATH"/"$XCODE_THEME_NAME"
+
 brew bundle install
 
 set +v
